841ade783310677e669ffeb6588f0500ebb47215
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2023-09-12 17:27:40 +0330

    burger-menu

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+const Ul = styled.ul `
+    list-style: none;
+    display: flex;
+    justify-content: right;
+    margin: 20px 50px;
+    z-index: 15;
+    li {
+        padding:18px 30px;
+    }
+
+    @media(max-width: 728px){
+        margin:0;
+        flex-direction: column;
+        background: #1567df ;
+        position: fixed;
+        transform: ${props => props.open ? 'transtlateX(0)' : 'translateX(100%)'}
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 300px;
+        padding-top: 3.5rem; 
+        li{
+            color:#fff;
+        }
+    }
+
+`
+
+const Navbar = ({props}) => {
+    return (
+        <div>
+            <Ul open={props} >
+                <li>Home</li>
+                <li>About</li>
+                <li>Github</li>
+                <li>Exit</li>
+            </Ul>
+        </div>
+    );
+};
+
+export default Navbar;
